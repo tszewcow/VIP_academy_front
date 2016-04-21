cb76b10e4657e7d5d33ce33f3f6e99e64ee2f75a
Tomasz Szewcow <tomasz.szewcow@capgemini.com>
2016-04-21 22:24:42 +0200

    clean up

--- app/app.module.js
+++
@@ -1,5 +0,0 @@
-angular.module('app', ['ngRoute',  'app.main', 'app.books-management'])
-    .config(function ($locationProvider) {
-        'use strict';
-        $locationProvider.html5Mode(false);
-    });
--- app/main/main.module.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.main', ['ngRoute', 'app.main.templates'])
-    .config(function ($routeProvider) {
-        'use strict';
-        $routeProvider
-            .when('/', {redirectTo: '/books-management/books-list'})
-            .when('/books-management/books-list', {templateUrl: 'books-management/books-list/books-list.html'})
-            .otherwise({templateUrl: 'main/page-not-found/page-not-found.html'});
-    });
